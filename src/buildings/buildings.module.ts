ceefe4cda3f207ef1e1a851d81da1712f202ef25
billyzporter <billy@zporter.co>
2022-12-20 14:11:31 +0700

    fix entity to building

--- src/buildings/buildings.module.ts
+++ src/buildings/buildings.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { BuildingsService } from './buildings.service';
 import { BuildingsController } from './buildings.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Building } from './entities/building.entity';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Building]), JwtModule],
+  imports: [JwtModule],
   providers: [BuildingsService],
   controllers: [BuildingsController],
 })
